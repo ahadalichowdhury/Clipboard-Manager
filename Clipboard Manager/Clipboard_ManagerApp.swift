863c13c072011ae86314538732a2bd67dd58f146
S. M. Ahad Ali Chowdhury <smahadalichowdhury@gmail.com>
2025-03-07 12:29:01 +0600

    Initial Commit

---
+++ Clipboard Manager/Clipboard_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clipboard_ManagerApp.swift
+//  Clipboard Manager
+//
+//  Created by S. M. Ahad Ali Chowdhury on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Clipboard_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
